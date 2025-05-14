54a98487152739fc2c7546718381314c042c7dc7
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2025-05-14 10:47:28 +0300

    First Training

--- CopyCode.py
+++ CopyCode.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
+ROWS = 7500  # Number of rows to copy
 input_file = 'Data/Images_Batched.csv'   # Replace with your source CSV filename
-output_file = 'CopiedFirst1000Rows.csv'  # Output file name
+output_file = f'CopiedFirst{ROWS}Rows.csv'  # Output file name
 
-df = pd.read_csv(input_file, nrows=1000) # Choose how many Rows
+df = pd.read_csv(input_file, nrows=ROWS)
 
 df.to_csv(output_file, index=False)
 
-print(f"First 1000 rows copied to {output_file}")
+print(f"First {ROWS} rows copied to {output_file}")
